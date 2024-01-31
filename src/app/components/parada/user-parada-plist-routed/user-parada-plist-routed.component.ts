130ba81416df83de230b97bd65441e8542617433
Paula <pauula.mnzz@gmail.com>
2024-01-31 18:25:56 +0100

    filtro paradas done

--- src/app/components/parada/user-parada-plist-routed/user-parada-plist-routed.component.ts
+++ src/app/components/parada/user-parada-plist-routed/user-parada-plist-routed.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiEmtService } from '../../../services/api-emt.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-parada-plist-routed',
   templateUrl: './user-parada-plist-routed.component.html',
-  styleUrls: ['./user-parada-plist-routed.component.css']
+  styleUrls: ['./user-parada-plist-routed.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+
 })
 export class UserParadaPlistRoutedComponent implements OnInit {
 paradas: string[] = [];
-
+searchTerm: string = '';
+filterParadas: string[] = [];
 
 
   constructor(
@@ -18,8 +27,17 @@ paradas: string[] = [];
   ngOnInit() {
     this.ApiEmtService.getAllParadas().subscribe(result => {
       this.paradas = result;
+      this.filterParadas = result;
       console.log(result);
     });
   }
-
+  search() {
+    if (this.searchTerm) {
+      this.paradas = this.filterParadas.filter(parada =>
+        parada.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.paradas = this.filterParadas;
+    }
+  }
 }
