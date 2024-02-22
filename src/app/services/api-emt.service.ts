a53b5a9cb8b713643963657b542d4b19ecca14f6
Paula <pauula.mnzz@gmail.com>
2024-02-22 01:52:57 +0100

    corregido

--- src/app/services/api-emt.service.ts
+++ src/app/services/api-emt.service.ts
@@ -23,104 +23,62 @@ private apiKey: string= API_KEY
     return this.oHttpClient.get(this.apiUrl);
   }
 
-  // Método para obtener todas las líneas de la EMT PAGINADAS
+
+
+
+
+  //ORDENADO
   getAllLineas(): Observable<string[]> {
     const apiUrl = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/emt/records?select=lineas&limit=100';
     const totalResults = 1129; // Total de resultados
     const requestsPerPage = 100; // Resultados por página
     const totalPages = Math.ceil(totalResults / requestsPerPage); // Total de páginas
-  
+
     // Crea un array de observables, uno para cada página
     const observables = Array.from({ length: totalPages }, (_, i) =>
       this.oHttpClient.get<IRoot>(`${apiUrl}&start=${i * requestsPerPage + 1}`)
     );
-  
+
     // Combina todos los observables en uno
     return forkJoin(observables).pipe(
       map((responses: any[]) => {
         // Combina todos los resultados en un solo array
         let results = ([] as IResultApi[]).concat(...responses.map(response => response.results));
-        
+
         // Aplana el array y elimina los duplicados
         let lineasUnicas = [...new Set(results.flatMap(result => result.lineas.split(',')))];
-    
+
+        // Ordena los resultados de mayor a menor
+        lineasUnicas.sort((a, b) => parseFloat(b) - parseFloat(a));
+
         return lineasUnicas;
       })
     );
   }
 
 
-
-  //ORDENADO
-  // getAllLineas(): Observable<string[]> {
-  //   const apiUrl = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/emt/records?select=lineas&limit=100';
-  //   const totalResults = 1129; // Total de resultados
-  //   const requestsPerPage = 100; // Resultados por página
-  //   const totalPages = Math.ceil(totalResults / requestsPerPage); // Total de páginas
-
-  //   // Crea un array de observables, uno para cada página
-  //   const observables = Array.from({ length: totalPages }, (_, i) =>
-  //     this.oHttpClient.get<IRoot>(`${apiUrl}&start=${i * requestsPerPage + 1}`)
-  //   );
-
-  //   // Combina todos los observables en uno
-  //   return forkJoin(observables).pipe(
-  //     map((responses: any[]) => {
-  //       // Combina todos los resultados en un solo array
-  //       let results = ([] as IResultApi[]).concat(...responses.map(response => response.results));
-
-  //       // Aplana el array y elimina los duplicados
-  //       let lineasUnicas = [...new Set(results.flatMap(result => result.lineas.split(',')))];
-
-  //       // Ordena los resultados de mayor a menor
-  //       lineasUnicas.sort((a, b) => parseFloat(b) - parseFloat(a));
-
-  //       return lineasUnicas;
-  //     })
-  //   );
-  // }
-
-
   //ORDENAO
-  // getAllParadas(): Observable<IResultApi[]> {
-  //   const apiUrl = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/emt/records';
-  //   const orderBy = 'id_parada ASC'; // Ordenar por ID de parada ascendente
-  //   const limit = 100; // Resultados por página
-  //   const totalResults = 1129; // Total de resultados
-  //   const totalPages = Math.ceil(totalResults / limit); // Total de páginas
+  getAllParadas(): Observable<IResultApi[]> {
+    const apiUrl = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/emt/records';
+    const orderBy = 'id_parada ASC'; // Ordenar por ID de parada ascendente
+    const limit = 100; // Resultados por página
+    const totalResults = 1129; // Total de resultados
+    const totalPages = Math.ceil(totalResults / limit); // Total de páginas
 
-  //   // Crea un array de observables, uno para cada página
-  //   const observables = Array.from({ length: totalPages }, (_, i) =>
-  //     this.oHttpClient.get<IRoot>(`${apiUrl}?order_by=${orderBy}&limit=${limit}&start=${i * limit + 1}`)
-  //   );
+    // Crea un array de observables, uno para cada página
+    const observables = Array.from({ length: totalPages }, (_, i) =>
+      this.oHttpClient.get<IRoot>(`${apiUrl}?order_by=${orderBy}&limit=${limit}&start=${i * limit + 1}`)
+    );
 
-  //   // Combina todos los observables en uno
-  //   return forkJoin(observables).pipe(
-  //     map((responses: any[]) => {
-  //       // Combina todos los resultados en un solo array
-  //       return ([] as IResultApi[]).concat(...responses.map(response => response.results));
-  //     })
-  //   );
-  // }
-getAllParadas(): Observable<IResultApi[]> {
-  const apiUrl = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/emt/records?order_by=id_parada%20ASC&limit=100';
-  const totalResults = 1129; // Total de resultados
-  const requestsPerPage = 100; // Resultados por página
-  const totalPages = Math.ceil(totalResults / requestsPerPage); // Total de páginas
+    // Combina todos los observables en uno
+    return forkJoin(observables).pipe(
+      map((responses: any[]) => {
+        // Combina todos los resultados en un solo array
+        return ([] as IResultApi[]).concat(...responses.map(response => response.results));
+      })
+    );
+  }
 
-  // Crea un array de observables, uno para cada página
-  const observables = Array.from({ length: totalPages }, (_, i) =>
-    this.oHttpClient.get<IRoot>(`${apiUrl}&start=${i * requestsPerPage + 1}`)
-  );
-
-  // Combina todos los observables en uno
-  return forkJoin(observables).pipe(
-    map((responses: any[]) => {
-      // Combina todos los resultados en un solo array
-      return ([] as IResultApi[]).concat(...responses.map(response => response.results));
-    })
-  );
-}
 
 getOneParada(paradaId: number): Observable<any> {
   const apiUrl = `${this.apiUrl}?where=id_parada=${paradaId}&limit=1`;
